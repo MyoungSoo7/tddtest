12ea4db9ee1b20ab63a5579d7401ca054ef18032
iamipro <iamipro@naver.com>
2024-08-27 10:32:30 +0900

    tdd

---
+++ src/main/java/me/lms/tddtest/TddtestApplication.java
@@ -0,0 +1,13 @@
+package me.lms.tddtest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TddtestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TddtestApplication.class, args);
+    }
+
+}
---
+++ src/test/java/me/lms/tddtest/TddtestApplicationTests.java
@@ -0,0 +1,13 @@
+package me.lms.tddtest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TddtestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
